7ee210c6a2935bfdd407b46a24cb238261809a24
GREADISE <2275417158@qq.com>
2023-05-19 14:25:12 +1000

    remove print and console log

--- loginApp/views.py
+++ loginApp/views.py
@@ -9,7 +9,7 @@ def login_check(request):
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
-        print(username)
+        #print(username)
         if user is not None:
             login(request, user)
             return redirect('welcome')
